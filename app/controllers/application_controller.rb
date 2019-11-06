fa8742334de9108140e132bc09f1671ed793ed2e
Neah Nieto <nrpcnieto@gmail.com>
2019-11-05 20:16:59 -0600

    Add action to controller to extract information from database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
+  def get_db_extract
+    @db_extract = Product.all
+  end
+
   def display_all_products
+    get_db_extract
     render "all_products.json.jb"
   end
 end
