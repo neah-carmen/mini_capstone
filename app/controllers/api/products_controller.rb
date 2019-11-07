2a2a104de543bb8048e7bc31f4c6def592592514
Neah Nieto <nrpcnieto@gmail.com>
2019-11-06 20:31:33 -0600

    Removed routes for first, second, and third product and their corresponding actions in the controller.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -25,24 +25,6 @@ class Api::ProductsController < ApplicationController
     render "product.json.jb"
   end
 
-  def display_first_product
-    parse_all_products
-    @all_products[0]
-    render "first_product.json.jb"
-  end
-
-  def display_second_product
-    parse_all_products
-    @all_products[1]
-    render "second_product.json.jb"
-  end
-
-  def display_third_product
-    parse_all_products
-    @all_products[2]
-    render "third_product.json.jb"
-  end
-
   def display_all_products
     parse_all_products
     render "all_products.json.jb"
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/product/:id" => "products#display_product"
-    get "/first_product" => "products#display_first_product"
-    get "/second_product" => "products#display_second_product"
-    get "/third_product" => "products#display_third_product"
     get "/all_products" => "products#display_all_products"
   end
 end
