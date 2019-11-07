68426cfa28f3a0fc3c710a96f550956f346052db
Neah Nieto <nrpcnieto@gmail.com>
2019-11-07 10:13:37 -0600

    Rewrote action controller to select products based on product id and not based off of array index

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,9 +5,10 @@ class Api::ProductsController < ApplicationController
   end
 
   def parse_all_products
-    db_extract = get_db_extract
+    db_extract = get_db_extract()
     @all_products = db_extract.map do |product|
       {
+        id: product.id,
         name: product.name,
         price: "#{product.currency_code}#{product.price}",
         stock: product.stock,
@@ -19,14 +20,16 @@ class Api::ProductsController < ApplicationController
   end
 
   def display_product
-    parse_all_products
-    @id = params["id"].to_i
-    @all_products[@id]
+    all_products = parse_all_products()
+    id = params["id"].to_i
+    @product = all_products.select do |product|
+      product[:id] == id
+    end
     render "product.json.jb"
   end
 
   def display_all_products
-    parse_all_products
+    parse_all_products()
     render "all_products.json.jb"
   end
 end
