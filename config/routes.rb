6a7e04d3dce5908ec6d1ff584f738a8df9964a1c
Neah Nieto <nrpcnieto@gmail.com>
2019-11-06 21:20:02 -0600

    Add route to routes.rb for query_param

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   namespace :api do
+    get "/product" => "products#display_product"
     get "/product/:id" => "products#display_product"
     get "/all_products" => "products#display_all_products"
   end
