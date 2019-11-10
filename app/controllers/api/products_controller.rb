6ee4c33f2cc069073a9f48fd13314e365269fd8d
Neah Nieto <nrpcnieto@gmail.com>
2019-11-10 13:14:24 -0600

    Reordered actions in controller to match order in routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -17,25 +17,6 @@ class Api::ProductsController < ApplicationController
         sale: product.on_sale,
       }
     end
-
-    def update
-      @product_by_id = Product.find_by(id: params[:id])
-      @product_by_id.name = params[:name] || @product_by_id.name
-      @product_by_id.price = params[:price] || @product_by_id.price
-      @product_by_id.currency_code = params[:currency_code] || @product_by_id.currency_code
-      @product_by_id.stock = params[:stock] || @product_by_id.stock
-      @product_by_id.image_url = params[:image_url] || @product_by_id.image_url
-      @product_by_id.description = params[:description] || @product_by_id.description
-      @product_by_id.on_sale = params[:on_sale] || @product_by_id.on_sale
-      @product_by_id.save
-      render "show.json.jb"
-    end
-
-    def destroy
-      @product_by_id = Product.find_by(id: params[:id])
-      @product_by_id.destroy
-      render json: { message: "Product successfully deleted." }
-    end
   end
 
   def index
@@ -65,5 +46,24 @@ class Api::ProductsController < ApplicationController
     end
     @product_by_id = @product_by_id[0]
     render "show.json.jb"
+  end
+
+  def update
+    @product_by_id = Product.find_by(id: params[:id])
+    @product_by_id.name = params[:name] || @product_by_id.name
+    @product_by_id.price = params[:price] || @product_by_id.price
+    @product_by_id.currency_code = params[:currency_code] || @product_by_id.currency_code
+    @product_by_id.stock = params[:stock] || @product_by_id.stock
+    @product_by_id.image_url = params[:image_url] || @product_by_id.image_url
+    @product_by_id.description = params[:description] || @product_by_id.description
+    @product_by_id.on_sale = params[:on_sale] || @product_by_id.on_sale
+    @product_by_id.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @product_by_id = Product.find_by(id: params[:id])
+    @product_by_id.destroy
+    render json: { message: "Product successfully deleted." }
   end
 end
