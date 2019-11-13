bbb8c28cfcde67b64a3a94421b77d346ff48d929
Neah Nieto <nrpcnieto@gmail.com>
2019-11-12 21:01:37 -0600

    Added validation for name, price, and description to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 1 }
+  validates :description, length: { in: 1..512 }
+
   def on_sale?
     @products = []
     db_extract = Product.all
