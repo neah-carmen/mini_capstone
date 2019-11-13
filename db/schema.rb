633c49f263668edb6fec77aca73078bf42a446d9
Neah Nieto <nrpcnieto@gmail.com>
2019-11-12 19:58:01 -0600

    Migrate Product.description from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_014929) do
+ActiveRecord::Schema.define(version: 2019_11_13_015546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_13_014929) do
     t.string "currency_code"
     t.integer "stock"
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.boolean "on_sale"
     t.integer "discount"
     t.datetime "created_at", precision: 6, null: false
