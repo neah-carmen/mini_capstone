cedfbdd362a6b9ab909543238913ac4aa3b78694
Neah Nieto <nrpcnieto@gmail.com>
2019-11-13 21:33:49 -0600

    Added skeleton for params search by discount to index action in controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,7 +5,7 @@ class Api::ProductsController < ApplicationController
     if params[:search]
       @products = @products.where("name ILIKE ?", "%#{params[:search]}%")
     elsif params[:sale_search]
-      @products = @products.where("on_sale = ?", "#{params[:sale_search]}")
+      @products = @products.where("on_sale = true", "%#{params[:sale_search]}%")
     end
 
     render "index.json.jb"
