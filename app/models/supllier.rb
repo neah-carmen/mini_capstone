6e7842998e7d12325697ea70da97e0c20e9d1082
Neah Nieto <nrpcnieto@gmail.com>
2019-11-14 21:07:11 -0600

    Create new Supplier model

---
+++ app/models/supllier.rb
@@ -0,0 +1,2 @@
+class Supllier < ApplicationRecord
+end
---
+++ db/migrate/20191115030626_create_suplliers.rb
@@ -0,0 +1,11 @@
+class CreateSuplliers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :suplliers do |t|
+      t.string :name
+      t.string :email
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
