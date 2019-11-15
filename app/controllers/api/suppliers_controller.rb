1fe06d55e04a181793404a02bec130603635fa5f
Neah Nieto <nrpcnieto@gmail.com>
2019-11-15 10:19:27 -0600

    Added the basic framework for RESTful actions in suppliers_controller.rb

--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -1,2 +1,24 @@
 class Api::SuppliersController < ApplicationController
+  def index
+    render json: { message: "index" }
+  end
+
+  def create
+    render json: { message: "create" }
+  end
+
+  def show
+    render json: { message: "show" }
+  end
+
+  def update
+    render json: { message: "update" }
+  end
+
+  def destroy
+    render json: { message: "destroy" }
+  end
+
+  def method_name
+  end
 end
