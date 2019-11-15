edce9145fca3081944cc4c6f611e324fd58f3a30
Neah Nieto <nrpcnieto@gmail.com>
2019-11-15 15:01:17 -0600

    Added more data entries to db/seeds.rb and added new suppplier_id attribute for seeded products

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,60 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-product = Product.create(
-  name: "Petticoat",
-  price: "49.99",
+Product.create(
+  name: "Pannier",
+  price: "149.99",
   currency_code: "$",
-  stock: "2",
-  image_url: "https://res.cloudinary.com/puereveryday/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1573415393/actualize/v1gvg9xicsno8hmvkw9o.jpg",
-  description: "Wear underneath skirt to add body.",
+  stock: "3",
+  image_url: "https://res.cloudinary.com/puereveryday/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1573851107/actualize/exeasjpifjjfjw6mbld0.png",
+  description: "Elasiticized waistband, full, gathered body. 100% Polyester.",
   on_sale: false,
   discount: nil,
+  supplier_id: 2,
 )
-product = Product.create(
-  name: "Blouse",
-  price: "249.99",
+Product.create(
+  name: "Petit Frill Blouse",
+  price: "149.99",
   currency_code: "$",
-  stock: "9",
-  image_url: "https://res.cloudinary.com/puereveryday/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1573415877/actualize/s9ytfrm1hu7a8iyroa28.jpg",
-  description: "Long sleeve blouse with standing collar",
+  stock: 4,
+  image_url: "https://res.cloudinary.com/puereveryday/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1573849944/actualize/yluscm3tgbrbkxvop3pu.png",
+  description: "Long sleeve blouse with frilled bib. 100% Cotton Broadcloth",
   on_sale: false,
   discount: nil,
+  supplier_id: 2,
 )
-product = Product.create(
-  name: "Circle Skirt",
-  price: "19.99",
+Product.create(
+  name: "La Floraison Feerique Lace Dress",
+  price: "349.99",
   currency_code: "$",
-  stock: "4",
-  image_url: "https://res.cloudinary.com/puereveryday/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1573415394/actualize/j8soywum2jdee3rv2jbt.jpg",
-  description: "Calf length full circle skirt",
+  stock: 2,
+  image_url: "https://res.cloudinary.com/puereveryday/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1573850570/actualize/y09nwsjwyjlmmm95vmi0.png",
+  description: "Half sleeve dress, rayon/polyester blend body with ribbon and lace accents.",
+  on_sale: false,
+  discount: nil,
+  supplier_id: 2,
+)
+Product.create(
+  name: "Pearl Diamonds Knitting Bolero",
+  price: "199.99",
+  currency_code: "$",
+  stock: 4,
+  image_url: "https://res.cloudinary.com/puereveryday/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1573850926/actualize/kj8anutabhzgfkq8cwmf.png",
+  description: "Wool/acrylic knitted half sleeve bolero.",
   on_sale: true,
-  discount: 10,
+  discount: 5,
+  supplier_id: 2,
+)
+Product.create(
+  name: "Maria Rosa Mistica Dress",
+  price: "449.99",
+  currency_code: "$",
+  stock: 2,
+  image_url: "https://res.cloudinary.com/puereveryday/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1573851106/actualize/c2jm5nteef29ti3msipm.png",
+  description: "Long sleeved dress, linen body with ribbon accents and lace trim.",
+  on_sale: false,
+  discount: nil,
+  supplier_id: 2,
 )
 Supplier.create(
   name: "Moi Meme Moitie",
