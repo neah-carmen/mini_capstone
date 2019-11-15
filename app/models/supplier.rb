4d68247f86e9fb5e610c596731aee4a22b3792fe
Neah Nieto <nrpcnieto@gmail.com>
2019-11-15 10:48:47 -0600

    Added name, email, and phone_number validations to the supplier model

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,6 @@
 class Supplier < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone_number, presence: true
 end
