74524f3d12ce50abbf674b9c5694e1fa0c089be5
Neah Nieto <nrpcnieto@gmail.com>
2019-11-17 09:46:36 -0600

    Add new method to Supplier model to find products that have corresponding supplier_id

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -3,4 +3,8 @@ class Supplier < ApplicationRecord
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :phone_number, presence: true
+
+  def products
+    Product.where(supplier_id: id)
+  end
 end
