4b309f0c9bd706f8dcb5f32671ec91fb6692cadd
Neah Nieto <nrpcnieto@gmail.com>
2019-11-17 11:07:56 -0600

    Remove old actions to create and update image_url attribute that was removed

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -33,7 +33,6 @@ class Api::ProductsController < ApplicationController
       price: params[:price],
       currency_code: params[:currency_code],
       stock: params[:stock],
-      image_url: params[:image_url],
       description: params[:description],
       on_sale: params[:on_sale],
       discount: params[:discount],
@@ -57,7 +56,6 @@ class Api::ProductsController < ApplicationController
     @product.price = params[:price] || @product.price
     @product.currency_code = params[:currency_code] || @product.currency_code
     @product.stock = params[:stock] || @product.stock
-    @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
     @product.on_sale = params[:on_sale] || @product.on_sale
     @product.discount = params[:discount] || @product.discount
