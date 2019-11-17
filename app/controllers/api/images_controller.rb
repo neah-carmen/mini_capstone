1752ff6b18184a58dd3291daead0d87bab8c13e2
Neah Nieto <nrpcnieto@gmail.com>
2019-11-17 11:31:21 -0600

    Create skeleton api CURD actions for images_controller.rb

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,2 +1,21 @@
 class Api::ImagesController < ApplicationController
+  def index
+    render json: { message: "index" }
+  end
+
+  def create
+    render json: { message: "create" }
+  end
+
+  def show
+    render json: { message: "show" }
+  end
+
+  def update
+    render json: { message: "update" }
+  end
+
+  def destroy
+    render json: { message: "destroy" }
+  end
 end
