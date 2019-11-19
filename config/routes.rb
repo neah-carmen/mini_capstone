cdbb1b8248a672f0855fa34d071d60bbdcf586fe
Neah Nieto <nrpcnieto@gmail.com>
2019-11-18 20:43:25 -0600

    Generate new User model

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
     get "/products/:id" => "products#show"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#destroy"
+
     get "/suppliers" => "suppliers#index"
     post "/suppliers" => "suppliers#create"
     get "/suppliers/:id" => "suppliers#show"
     patch "/suppliers/:id" => "suppliers#update"
     delete "/suppliers/:id" => "suppliers#destroy"
+
     get "/images" => "images#index"
     post "/images" => "images#create"
     get "/images/:id" => "images#show"
