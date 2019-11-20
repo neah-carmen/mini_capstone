6930207c46d9eb289c7e9b406b9634093ba9bf6c
Neah Nieto <nrpcnieto@gmail.com>
2019-11-19 21:06:12 -0600

    Add route for show order and add skeleton for show action for orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
 
     get "/orders" => "orders#index"
     post "/orders" => "orders#create"
+    get "/orders/:id" => "orders#show"
 
     post "/users" => "users#create"
 
