d7d71bde399498244db29caf5f3fa6c94a2ae70e
Neah Nieto <nrpcnieto@gmail.com>
2019-11-20 19:07:21 -0600

    Refactored create action for orders controller and used authentication

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,39 +1,34 @@
 class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    if current_user
-      @orders = Order.where(user_id: current_user.id)
-      render "index.json.jb"
-    else
-      render json: []
-    end
+    @orders = Order.where(user_id: current_user.id)
+    render "index.json.jb"
   end
 
   def create
-    if current_user
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-      )
-      if @order.save
-        @product = Product.find_by(id: params[:product_id])
-        @order.subtotal = @order.quantity * @product.price
-        @order.tax = @order.quantity * @product.tax
-        @order.total = @order.quantity * @product.total
-        @order.save
-        render "show.json.jb"
-      end
+    @product = Product.find_by(id: params[:product_id])
+    subtotal = params[:quantity].to_i * @product.price
+    tax = params[:quantity].to_i * @product.tax
+    total = params[:quantity].to_i * @product.total
+
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: subtotal,
+      tax: tax,
+      total: total,
+    )
+    if @order.save
+      render "show.json.jb"
     else
-      render json: []
+      render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def show
-    if current_user
-      @order = Order.find_by(id: params[:id])
-      render "show.json.jb"
-    else
-      render json: []
-    end
+    @order = Order.find_by(id: params[:id])
+    render "show.json.jb"
   end
 end
