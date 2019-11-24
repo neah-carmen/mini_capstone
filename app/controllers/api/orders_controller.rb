b7a3dbabfc8004441b71fafc6395deec23b68a45
Neah Nieto <nrpcnieto@gmail.com>
2019-11-24 11:35:33 -0600

    Remove old code referring to old Product model association

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,12 +7,17 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    @product = Product.find_by(id: params[:product_id])
-    subtotal = params[:quantity].to_i * @product.price
-    tax = params[:quantity].to_i * @product.tax
-    total = params[:quantity].to_i * @product.total
+    shopping_cart = ShoppingCart.find_by(status: "Carted")
+    quantity = shopping_cart.quantity
+    product_id = shopping_cart.product_id
+
+    product = Product.find_by(id: product_id.to_i)
+    subtotal = quantity.to_i * product.price
+    tax = quantity.to_i * product.tax
+    total = quantity.to_i * product.total
 
     @order = Order.new(
+      user_id: current_user.id,
       subtotal: subtotal,
       tax: tax,
       total: total,
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
-  belongs_to :product
 
   has_many :shopping_carts
 end
