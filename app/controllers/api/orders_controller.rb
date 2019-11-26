e542e1cab70099445d99abd0cce3a31868594751
Neah Nieto <nrpcnieto@gmail.com>
2019-11-26 09:46:46 -0600

    Moved calculations out of create action for orders_controller and moved it in to order model

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,26 +7,17 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    shopping_cart = ShoppingCart.find_by(status: "Carted")
-    quantity = shopping_cart.quantity
-    product_id = shopping_cart.product_id
-
-    product = Product.find_by(id: product_id.to_i)
-    subtotal = quantity.to_i * product.price
-    tax = quantity.to_i * product.tax
-    total = quantity.to_i * product.total
-
     @order = Order.new(
       user_id: current_user.id,
-      subtotal: subtotal,
-      tax: tax,
-      total: total,
     )
+    @order.save
+
+    shopping_cart = current_user.shopping_carts.where(status: "Carted")
+    shopping_cart.update_all(status: "Purchased", order_id: @order.id)
+
+    @order.calculations
+
     if @order.save
-      shopping_cart = ShoppingCart.find_by(status: "Carted")
-      shopping_cart.status = "Purchased"
-      shopping_cart.order_id = @order.id
-      shopping_cart.save
       render "show.json.jb"
     else
       render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,20 @@ class Order < ApplicationRecord
 
   has_many :shopping_carts
   has_many :products, through: :shopping_carts
+
+  def calculations
+    cart_subtotal = 0
+    cart_taxes = 0
+
+    shopping_carts.each do |item|
+      cart_subtotal += (item.product.price * item.quantity)
+      cart_taxes += (item.product.tax * item.quantity)
+    end
+
+    cart_total = cart_subtotal + cart_taxes
+    self.subtotal = cart_subtotal
+    self.tax = cart_taxes
+    self.total = cart_total
+    self.save
+  end
 end
