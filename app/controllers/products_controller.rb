2a4cfc13a85e2bf24be1d3dece7c0b97741119c2
Neah Nieto <nrpcnieto@gmail.com>
2019-11-26 21:15:32 -0600

    Fleshed out the HTML create action for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,17 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      currency_code: params[:currency_code],
+      stock: params[:stock],
+      description: params[:description],
+      on_sale: params[:on_sale],
+      discount: params[:discount],
+      supplier_id: params[:supplier_id],
+    )
+    @product.save
     redirect_to "/recipes"
   end
 
