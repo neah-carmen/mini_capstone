49df93b805b7eb684b45fa23ba5b6b698ce0201e
Neah Nieto <nrpcnieto@gmail.com>
2019-11-26 20:54:15 -0600

    Add skeleton for HTML new action for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,10 @@ class ProductsController < ApplicationController
     render "index.html.erb"
   end
 
+  def new
+    render "new.html.erb"
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
     render "show.html.erb"
