e16b9c9d01aa51a8b27fd05267d7a41a170d7770
Neah Nieto <nrpcnieto@gmail.com>
2019-11-27 18:54:44 -0600

    Completed HTML update action for products controller with corresponding route and view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
     if @product.save
       redirect_to "/products/#{@product.id}"
     else
-      redirect_to "/products"
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -40,5 +40,28 @@ class ProductsController < ApplicationController
   def edit
     @product = Product.find_by(id: params[:id])
     render "edit.html.erb"
+  end
+
+  def update
+    if params[:on_sale] == "false"
+      discount = false
+    elsif params[:on_sale] == "true"
+      discount = true
+    end
+
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.currency_code = params[:currency_code]
+    @product.stock = params[:stock]
+    @product.description = params[:description]
+    @product.on_sale = discount
+    @product.discount = params[:discount]
+    @product.supplier_id = params[:supplier_id]
+    if @product.save
+      redirect_to "/products/#{@product.id}"
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post "/products" => "products#create"
   get "/products/:id" => "products#show"
   get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
 
   namespace :api do
     get "/products" => "products#index"
